b601f4e63ab17ad329fb6eaf194429e33189fded
NPCHAMPION <mike@CHAMP-BOX.localdomain>
2017-12-01 11:01:18 -0800

    added footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
+const mctLogo = require('../images/MCT-Logo.svg');
+
 class Footer extends Component {
-    state = {  }
+    state = { 
+        logo: mctLogo,
+     }
     render() {
         return (
-            <footer>footer</footer>
+            <footer>
+                <div className="footer-box">
+                    <p>Designed by </p>
+                    <a href="www.mctdev.org" target="_blank"><img src={ this.state.logo } /></a>
+                </div>
+            </footer>
         );
     }
 }
